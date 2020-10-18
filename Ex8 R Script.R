a5b2429e2815c3df90c1f66a2ec386ec39b7e57a
Michelle Huang <o.omichelley@gmail.com>
2020-10-17 20:59:04 -0400

    created vector with cumulative sums of each team's score

--- Ex8 R Script.R
+++ Ex8 R Script.R
@@ -4,7 +4,7 @@ scores <- read.csv("UWvMSU_1-22-13.txt", sep="", stringsAsFactors = FALSE)
 MSU <- 0
 UW <- 0
 
-# create for loop with if else statement
+# create for loop with if else statement (nevermind, do not run this!)
 for (i in 1:nrow(scores)){
   if (scores$team[i]=="UW"){
   UW <- UW+scores$score[i]
@@ -12,3 +12,18 @@ for (i in 1:nrow(scores)){
     MSU <- MSU+scores$score[i]
   }
 }
+
+# create new variables
+UWlist <- vector(length=0)
+MSUlist <- vector(length=0)
+# extract scores for UW and MSU
+for (i in 1:nrow(scores)){
+  if (scores$team[i]=="UW"){
+    UWlist <- c(UWlist, scores$score[i])
+  } else {
+    MSUlist <- c(MSUlist, scores$score[i])
+  }
+}
+# create vector with cumulative sums
+UWcumsum <- cumsum(UWlist)
+MSUcumsum <- cumsum(MSUlist)
