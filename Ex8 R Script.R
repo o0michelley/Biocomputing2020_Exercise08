e355ccdb9e8059ff693d025ccec2767e6f5607e5
Michelle Huang <o.omichelley@gmail.com>
2020-10-17 20:43:41 -0400

    created for loop

--- Ex8 R Script.R
+++ Ex8 R Script.R
@@ -1,2 +1,14 @@
 scores <- read.csv("UWvMSU_1-22-13.txt", sep="", stringsAsFactors = FALSE)
 
+# create variables
+MSU <- 0
+UW <- 0
+
+# create for loop with if else statement
+for (i in 1:nrow(scores)){
+  if (scores$team[i]=="UW"){
+  UW <- UW+scores$score[i]
+  } else {
+    MSU <- MSU+scores$score[i]
+  }
+}
